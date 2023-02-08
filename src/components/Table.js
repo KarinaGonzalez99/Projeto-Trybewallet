183a6020c77454de8a2de784676f74bf6845a186
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-02-08 19:17:02 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <th>Descrição</th>
+          <th>Moeda de conversão</th>
+          <th>Moeda</th>
+          <th>Valor</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Editar/Excluir</th>
+        </table>
+      </div>
     );
   }
 }
 
 export default Table;
+// A tabela possui um cabeçalho com elementos <th> com os valores Descrição, Tag, Método de pagamento,Valor, Moeda, Câmbio utilizado, Valor convertido, Moeda de conversão e Editar/Excluir.

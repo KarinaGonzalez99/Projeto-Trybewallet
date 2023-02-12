267873ebe5070983505d5815143dd38cf0ce357a
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-02-12 15:26:33 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props; // chamado da api
     return (
       <div>
         <table>
@@ -15,10 +16,49 @@ class Table extends Component {
           <th>Método de pagamento</th>
           <th>Editar/Excluir</th>
         </table>
+
+        <tbody>
+          {expenses.map((event) => (
+            <tr key={ event }>
+              <td>
+                {event.description}
+              </td>
+              <td>
+                {event.tag}
+              </td>
+              <td>
+                {event.method}
+              </td>
+              <td>
+                {Number(event.value).toFixed(2)}
+              </td>
+              <td>
+                {event.exchangeRates[event.currency].name}
+              </td>
+              <td>
+                {Number(event.exchangeRates[event.currency].ask).toFixed(2)}
+              </td>
+              <td>
+                {Number(event.exchangeRates[event.currency].ask).toFixed(2)}
+              </td>
+              <td>
+                {Number(event.exchangeRates[event.currency].ask * event.value).toFixed(2)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </div>
     );
   }
 }
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(),
+}.isRequired;
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
+
 // A tabela possui um cabeçalho com elementos <th> com os valores Descrição, Tag, Método de pagamento,Valor, Moeda, Câmbio utilizado, Valor convertido, Moeda de conversão e Editar/Excluir.
